849be39414a5b0a12443b2b301c3382fa964ad3c
SenorPenguin <jimmy.dwyer25@gmail.com>
2019-01-16 17:31:13 -0600

    Added friends info, added preliminary path-work, server side programming, no front-end jquery yet. I love you still, mom

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friends = [
+
+    {
+        name: "Zach",
+        pic: "https://i.imgur.com/eUIzTH9.png",
+        scores: [
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4"
+        ]
+    },
+    {
+        name: "Ryan",
+        pic: "https://i.pinimg.com/originals/d6/7d/a8/d67da81855704fd72ecd6b05dd640cec.jpg",
+        scores: [
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2"
+        ]
+    }
+
+
+]
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,51 @@
+//PSEUDOCODE AND OUTLINE TAKEN FROM HOT RESTAURANT
+
+
+
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friendsData = require("./app/data/friends.js");
+
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/friends", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+   var userdata=req.body;
+    for (i=0; i = friendsData.length; i++) {
+      friendsData[i].push(req.body);
+      
+      //gets the last object that was insert from the client form
+      res.json(friendsData[friendsData.length-1]);
+    }
+  });
+};
